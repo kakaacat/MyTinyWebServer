21215d9abb1537828957c0531ba510542e32f052
kakaacat <1912445179@qq.com>
2023-04-23 16:23:53 +0800

    log

---
+++ log/log.cpp
@@ -0,0 +1,71 @@
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdarg.h>
+#include "log.h"
+#include <pthread.h>
+
+using namespace std;
+
+Log::Log()
+{
+    m_count = 0;
+    m_is_async = false;
+}
+
+Log::~Log()
+{
+    if (m_fp != NULL)
+    {
+        fclose(m_fp);
+    }
+}
+
+//异步需要设置阻塞队列的长度，同步不需要设置
+bool Log::init(const char *file_name, int close_log,int log_buf_size, int split_lines, int max_queue_size)
+{
+    //如果设置了max_queue_size,则设置为异步
+    if (max_queue_size >= 1)
+    {
+        m_is_async = true;
+        m_log_queue = new block_queue<string>(max_queue_size);
+        pthread_t tid;
+        //flush_log_thread为回调函数,这里表示创建线程异步写日志
+        pthread_create(&tid, NULL, flush_log_thread, NULL);
+    }
+    
+    m_close_log = close_log;
+    m_log_buf_size = log_buf_size;
+    m_buf = new char[m_log_buf_size];
+    memset(m_buf, '\0', m_log_buf_size);
+    m_split_lines = split_lines;
+
+    time_t t = time(NULL);
+    struct tm *sys_tm = localtime(&t);
+    struct tm my_tm = *sys_tm;
+
+ 
+    const char *p = strrchr(file_name, '/');
+    char log_full_name[256] = {0};
+
+    if (p == NULL)
+    {
+        snprintf(log_full_name, 255, "%d_%02d_%02d_%s", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, file_name);
+    }
+    else
+    {
+        strcpy(log_name, p + 1);
+        strncpy(dir_name, file_name, p - file_name + 1);
+        snprintf(log_full_name, 255, "%s%d_%02d_%02d_%s", dir_name, my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, log_name);
+    }
+
+    m_today = my_tm.tm_mday;
+    
+    m_fp = fopen(log_full_name, "a");
+    if (m_fp == NULL)
+    {
+        return false;
+    }
+
+    return true;
+}
